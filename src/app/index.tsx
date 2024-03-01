3dc51548683d41b1b8c934011f65cf105cc07938
Nikita <belkinsrc@gmail.com>
2024-03-01 10:15:36 +0300

    chore: init project

---
+++ src/app/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 className="text-red-500">Star DB</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
